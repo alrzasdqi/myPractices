bcd0b0a01af6429c0ce577887b762c7d3a554f43
alrzasdqi <abaptistonz@gmail.com>
2019-05-06 11:22:13 +0430

    changed

--- rhombus.py
+++ rhombus.py
@@ -2,4 +2,4 @@ base = float(input("length of base: "))
 height = float(input("measurement of height: "))
 
 area = base * height
-print("Area is: " + str(area))
+print("Area is     :     " + str(area))
